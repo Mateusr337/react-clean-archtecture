60242d0ef6713ad8c5add0e5ce7df71cd577a177
Mateus Rossetto <mateusc.rossetto@hotmail.com>
2023-12-15 13:12:08 -0300

    test: sure AxiosHttpClient is called with the correct url and REST method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,16 +1,20 @@
 import { faker } from "@faker-js/faker";
-import { AxiosHttpClient } from ".";
 import axios from "axios";
+import { AxiosHttpClient } from ".";
 
 jest.mock("axios");
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient();
+};
+
 describe("AxiosHttpClient", () => {
-  test("Should Call axios with the correct URL", async () => {
+  test("Should Call axios with the correct URL and Verb", async () => {
     const url = faker.internet.url();
-    const sut = new AxiosHttpClient();
+    const sut = makeSut();
     await sut.post({ url });
-    expect(mockedAxios).toHaveBeenCalledWith(url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(url);
   });
 });
