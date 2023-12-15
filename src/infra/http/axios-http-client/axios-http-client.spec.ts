00cdc65e85a724d6d84ffdc805246816140755cf
Mateus Rossetto <mateusc.rossetto@hotmail.com>
2023-12-15 13:23:43 -0300

    refactor: move post request to a factory function

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,3 +1,4 @@
+import { HttpPostParams } from "@/data/protocols/http";
 import { faker } from "@faker-js/faker";
 import axios from "axios";
 import { AxiosHttpClient } from ".";
@@ -6,15 +7,27 @@ jest.mock("axios");
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: new Object(),
+});
+
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
 
 describe("AxiosHttpClient", () => {
-  test("Should Call axios with the correct URL and Verb", async () => {
-    const url = faker.internet.url();
+  test("Should Call axios with the correct URL and REST method", async () => {
+    const { url } = mockPostRequest();
     const sut = makeSut();
     await sut.post({ url });
     expect(mockedAxios.post).toHaveBeenCalledWith(url);
   });
+
+  //   test("Should Call axios with the correct body", async () => {
+  //     const request = mockPostRequest();
+  //     const sut = makeSut();
+  //     await sut.post({ url: faker.internet.url() });
+  //     expect(mockedAxios.post).toHaveBeenCalledWith(url);
+  //   });
 });
